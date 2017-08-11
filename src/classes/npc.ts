0f7cead7482f978ca196b2b53d7cc9f247e9092f
fcalabria <pcalabria@gmail.com>
2017-08-11 21:42:49 +0200

    feat(Character creation): Changed habilities
    Same list of habilities for above and below average
    
    3

--- src/classes/npc.ts
+++ src/classes/npc.ts
@@ -1,11 +1,19 @@
+interface customProp extends Object {
+  type: string,
+  max: number,
+  empty: number,
+  maxValues: number
+}
 export class NPC {
   [key: string]: any;
-  propsParams = new Map([
-    ['genre', { type: 'number', max: 2, empty: 0 }],
-    ['mark', { type: 'number', max: 10, empty: 0.5 }],
-    ['archetype', { type: 'number', max: 12, empty: 0 }],
-    ['weakness', { type: 'number', max: 10, empty: 0 }],
-    ['virtue', { type: 'number', max: 10, empty: 0 }],
+  propsParams: Map<string, customProp> = new Map([
+    ['genre', { type: 'number', max: 2, empty: 0, maxValues: null }],
+    ['mark', { type: 'number', max: 10, empty: 0.5, maxValues: null }],
+    ['archetype', { type: 'number', max: 12, empty: 0, maxValues: null }],
+    ['weakness', { type: 'number', max: 10, empty: 0, maxValues: null }],
+    ['virtue', { type: 'number', max: 10, empty: 0, maxValues: null }],
+    ['above_avg', { type: 'numsArray', max: 17, empty: 0, maxValues: 4 }],
+    ['below_avg', { type: 'numsArray', max: 17, empty: 0, maxValues: 4 }],
   ]);
   constructor() {
     const empty = {
@@ -30,17 +38,17 @@ export class NPC {
         below_avg: <number[]>[]
       },
     }
-    return this.randomizeProperties(empty);
+    return this.dontRepeatHabilities(this.randomizeProperties(empty));
   }
 
   randomNumber(max: number, min: number = 0): number {
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
-  randomArrayOfNumbers(): number[] {
-    const totalNumbers = this.randomNumber(4, 1);
+  randomArrayOfNumbers({maxValues, biggerValue} = {maxValues: 0, biggerValue: 0}): number[] {
+    const totalNumbers = maxValues || this.randomNumber(4, 1);
     return Array
-      .from({ length: totalNumbers }, () => this.randomNumber(10))
+      .from({ length: totalNumbers }, () => this.randomNumber(biggerValue || 10))
       // Filter repeated numbers
       .filter((n, i, a) => a.indexOf(n) === i);
   }
@@ -52,7 +60,7 @@ export class NPC {
       case 'number':
         return this.randomNumber(config.max);
       case 'numsArray':
-        return this.randomArrayOfNumbers();
+        return this.randomArrayOfNumbers({maxValues: config.maxValues, biggerValue: config.max});
 
       default:
         console.error(`Not known type ${config.type} for ${prop}`);
@@ -67,6 +75,18 @@ export class NPC {
       case 'object':
         return this.randomArrayOfNumbers();
     }
+  }
+
+  dontRepeatHabilities(npc: NPC): NPC {
+    const aboveavg: number[] = npc.habilities.above_avg;
+    const belowavg = npc.habilities.below_avg;
+    npc.habilities.above_avg = aboveavg.map((value, i) => {
+      while (belowavg.includes(value) || (aboveavg.indexOf(value) !== -1 && aboveavg.indexOf(value) < i)) {
+        value = this.randomNumber(this.propsParams.get('below_avg').max);
+      }
+      return value;
+    });
+    return npc;
   }
 
   randomizeProperties(nestedObject: { [key: string]: any }): NPC {
